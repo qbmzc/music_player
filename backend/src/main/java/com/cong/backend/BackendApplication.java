af6aceac0f78d209f059b11aea3f5bd9796747c8
cong <congco@foxmail.com>
2024-12-07 17:52:01 +0800

    music

---
+++ backend/src/main/java/com/cong/backend/BackendApplication.java
@@ -0,0 +1,16 @@
+package com.cong.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cong
+ * @date 2024/12/7 17:42
+ */
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
---
+++ backend/src/main/java/com/cong/backend/controller/S3Controller.java
@@ -0,0 +1,14 @@
+package com.cong.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author cong
+ * @date 2024/12/7 17:43
+ */
+@RestController
+@RequestMapping("api")
+public class S3Controller {
+
+}
