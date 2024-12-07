b7e56dc71c85b1bd12763f67d4851aa882aeb24d
cong <congco@foxmail.com>
2024-12-07 18:27:44 +0800

    music

---
+++ backend/src/main/java/com/cong/backend/model/Music.java
@@ -0,0 +1,26 @@
+package com.cong.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+/**
+ * @author cong
+ * @date 2024/12/7 18:24
+ */
+@Data
+@Entity
+@Table(name = "t_music")
+public class Music {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fileUrl;
+    private String title;
+    private String author;
+    private String album;
+    private String genre;
+    private String path;
+    private String description;
+}
---
+++ backend/src/main/java/com/cong/backend/service/S3Service.java
@@ -0,0 +1,15 @@
+package com.cong.backend.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author cong
+ * @date 2024/12/7 18:24
+ */
+@Service
+@Slf4j
+public class S3Service {
+
+
+}
